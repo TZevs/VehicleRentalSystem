449ed9e358067b116696dfbc7fe6d1ed76a17346
Thalia <148969711+TZevs@users.noreply.github.com>
2024-10-05 10:21:10 +0100

    Project files created

---
+++ VehicleRentalApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VehicleRentalApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
