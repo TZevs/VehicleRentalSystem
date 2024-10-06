77acc8995a5d729b96b462ef29d5c5b1bfc9ef27
Thalia <148969711+TZevs@users.noreply.github.com>
2024-10-06 13:05:39 +0100

    Vehicle Class Page created and class declared.

---
+++ VehicleRentalApp/Vehicles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalApp
+{
+    internal class Vehicles
+    {
+        public string Make;
+        public string Model;
+        public string Year;
+        public decimal DailyRate;
+        public bool IsAvailable = true;
+    }
+}
