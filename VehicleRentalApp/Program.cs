eb3ab0049c45ae3cfbd1b25f8a8604829d01927d
Thalia <148969711+TZevs@users.noreply.github.com>
2024-10-08 17:47:42 +0100

    Hardcoded object examples into collection. Switch for user menu selection.

--- VehicleRentalApp/Program.cs
+++ VehicleRentalApp/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Dictionary<int, Vehicle> vehicles = new Dictionary<int, Vehicle>();
-            
+            vehicles.Add(1, new Vehicle("FIAT", "500", 2014, 50, "Manual"));
+            vehicles.Add(2, new Vehicle("Mercedes", "A-Class", 2020, 60, "Automatic"));
+            vehicles.Add(3, new Vehicle("BMW", "1 Series", 2018, 69, "Automatic"));
+
+            //foreach (KeyValuePair<int, Vehicle> v in vehicles)
+            //{
+            //    string status = v.Value.IsAvailable == true ? "Available" : "Rented";
+            //    Console.WriteLine($"{v.Key}, {v.Value.Make}, {v.Value.Model}, {v.Value.Year}, {v.Value.DailyRate}, {v.Value.Transmission}, {status}");
+            //}
+
             void MainMenu()
             {
                 Console.WriteLine("VEHICLE RENTAL - MAIN MENU");
@@ -15,26 +24,40 @@
                 Console.WriteLine("[4] Delete Vehicle");
                 Console.WriteLine("[5] Exit");
 
-                Console.Write("Enter Selection: ");
-                int select = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    Console.Write("Enter Menu Option: ");
+                    string select = Console.ReadLine().Trim();
+                    switch (select)
+                    {
+                        case "1": ViewVehicles(); return;
+                        case "2": SearchVehicles(); return;
+                        case "3": AddVehicles(); return;
+                        case "4": DeleteVehicles(); return;
+                        default: Console.WriteLine("Please Enter a Menu Option"); break;
+                    }
+                }
             }
 
             void ViewVehicles()
             {
-
+                Console.Clear();
+                Console.WriteLine("ALL VEHICLES");
             }
             void SearchVehicles()
             {
-
+                Console.Clear();
             }
             void AddVehicles()
             {
-
+                Console.Clear();
             }
             void DeleteVehicles()
             {
-
+                Console.Clear();
             }
+
+            MainMenu();
         }
     }
 }
