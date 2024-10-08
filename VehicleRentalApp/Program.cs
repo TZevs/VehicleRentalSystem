d50623e9dd3e02612ff659641523ad07c6759ba7
Thalia <148969711+TZevs@users.noreply.github.com>
2024-10-08 12:02:58 +0100

    Main Menu Function Started

--- VehicleRentalApp/Program.cs
+++ VehicleRentalApp/Program.cs
@@ -6,6 +6,35 @@
         {
             Dictionary<int, Vehicle> vehicles = new Dictionary<int, Vehicle>();
             
+            void MainMenu()
+            {
+                Console.WriteLine("VEHICLE RENTAL - MAIN MENU");
+                Console.WriteLine("[1] View Vehicles");
+                Console.WriteLine("[2] Search Vehicles");
+                Console.WriteLine("[3] Add Vehicle");
+                Console.WriteLine("[4] Delete Vehicle");
+                Console.WriteLine("[5] Exit");
+
+                Console.Write("Enter Selection: ");
+                int select = Convert.ToInt32(Console.ReadLine());
+            }
+
+            void ViewVehicles()
+            {
+
+            }
+            void SearchVehicles()
+            {
+
+            }
+            void AddVehicles()
+            {
+
+            }
+            void DeleteVehicles()
+            {
+
+            }
         }
     }
 }
