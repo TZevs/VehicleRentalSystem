16ae56121421ef56f3a41a1956890e43058c71c5
Thalia <148969711+TZevs@users.noreply.github.com>
2024-10-13 13:01:16 +0100

    Add Vehicle function basics done. No validation

--- VehicleRentalApp/Program.cs
+++ VehicleRentalApp/Program.cs
@@ -52,27 +52,60 @@
                     Console.WriteLine($"{v.Key} || {v.Value.Make} || {v.Value.Model} || {v.Value.Year} || £{v.Value.DailyRate} || {v.Value.Transmission} || {status}");
                 }
                 
-                Console.WriteLine("[1] Back to Main || [2] Search Vehicles");
+                Console.WriteLine("[0] Back to Main || [2] Search Vehicles");
                 while (true)
                 {
                     Console.Write("Enter Option: ");
                     string select = Console.ReadLine().Trim();
                     switch (select)
                     {
-                        case "1": MainMenu(); return;
+                        case "0": MainMenu(); return;
                         case "2": SearchVehicles(); return;
                         default: break;
                     }
                 }
             }
+
             void SearchVehicles()
             {
                 Console.Clear();
             }
+
             void AddVehicles()
             {
                 Console.Clear();
+                Console.WriteLine("ADD VEHICLE");
+
+                // Need to add validation to inputs - Year and DailyRate will cause errors if wrong.
+                // Transmission type: Auto and Man - validate different inputs. Different for vehicle types. 
+                Console.Write("Make: ");
+                string make = Console.ReadLine();
+                Console.Write("Model: ");
+                string model = Console.ReadLine();
+                Console.Write("Year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Daily Rate: ");
+                decimal dailyRate = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Transmission Type: ");
+                string transmission = Console.ReadLine();
+
+                vehicles.Add(vehicles.Count() + 1, new Vehicle(make, model, year, dailyRate, transmission));
+                
+                Console.WriteLine("[0] Back to Main || [1] View Vehicles || [3] Add Another Vehicle");
+                while (true)
+                {
+                    Console.Write("Enter Option: ");
+                    string select = Console.ReadLine().Trim();
+                    switch (select)
+                    {
+                        case "0": MainMenu(); return;
+                        case "1": ViewVehicles(); return;
+                        case "3": AddVehicles(); return;
+                        default: break;
+                    }
+                }
             }
+
             void DeleteVehicles()
             {
                 Console.Clear();
