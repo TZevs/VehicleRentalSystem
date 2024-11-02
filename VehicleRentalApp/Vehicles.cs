57ff748b88ed60d81512386cc2dbe2225ad5c7a4
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-02 15:02:53 +0000

    Getters and a Setter

--- VehicleRentalApp/Vehicles.cs
+++ VehicleRentalApp/Vehicles.cs
@@ -8,6 +8,7 @@ namespace VehicleRentalApp
 {
     internal class Vehicle
     {
+        public int id;
         public string Make;
         public string Model;
         public int Year;
@@ -24,5 +25,21 @@ namespace VehicleRentalApp
             Transmission = transmission;
             Status = "Available";
         }
+
+        public void SetVehicle(string make, string model, int year, decimal dailyRate, string transmission, string status)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.DailyRate = dailyRate;
+            this.Transmission = transmission;
+            this.Status = status;
+        }
+        public string GetMake() { return Make; }
+        public string GetModel() { return Model; }
+        public int GetYear() { return Year; }
+        public decimal GetRate() { return DailyRate; }
+        public string GetTransmission() { return Transmission; }
+        public string GetStatus() { return Status; }
     }
 }
