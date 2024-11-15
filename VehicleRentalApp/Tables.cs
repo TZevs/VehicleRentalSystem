6857dbef0d52f10983e6913b32f7aa3d41feb258
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-15 09:07:04 +0000

    Started table class

---
+++ VehicleRentalApp/Tables.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalApp
+{
+    internal class Tables
+    {
+        private string[] Headers;
+        private Dictionary<int, Vehicle> Rows; 
+        private int[] ColumnWidths;
+
+        public Tables(string[] headers, Dictionary<int, Vehicle> rows)
+        {
+            Headers = headers;
+            Rows = rows;
+        }
+        private void SetColumnWidths()
+        {
+            ColumnWidths = new int[Headers.Length];
+            for (int i = 0; i < Headers.Length; i++)
+            {
+                ColumnWidths[i] = Headers[i].Length;
+                foreach (var row in Rows)
+                {
+                    
+                }
+            }
+        }
+        public void PrintTable()
+        {
+            
+        }
+    }
+}
