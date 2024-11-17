fb0e01975f581da1a35e3c0b793f46c696a9a431
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-17 15:24:32 +0000

    enum for errors

--- VehicleRentalApp/Errors.cs
+++ VehicleRentalApp/Errors.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace VehicleRentalApp
 {
-    internal enum Errors
+    enum ErrorType
     {
+        Info,
+        Warning,
+        Error,
+        Critical
+    }
 
+    internal class Errors
+    {
+        public string GetColor(ErrorType error)
+        {
+            switch (error)
+            {
+                case ErrorType.Info:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string infoError = "INFO: ";
+                    Console.ResetColor();
+                    return infoError;
+
+                case ErrorType.Warning:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    string warnError = "WARNING: ";
+                    Console.ResetColor();
+                    return warnError;
+
+                case ErrorType.Error:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    string errError = "ERROR: ";
+                    Console.ResetColor();
+                    return errError;
+
+                case ErrorType.Critical:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    string critError = "CRITICAL: ";
+                    Console.ResetColor();
+                    return critError;
+
+                default: return "Unknown Error";
+            }
+        }
     }
 }
