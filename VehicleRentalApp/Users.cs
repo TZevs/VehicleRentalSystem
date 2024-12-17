67cabe8ae1e13fb4897e6bd3f60d7774bc945312
Thalia <148969711+TZevs@users.noreply.github.com>
2024-12-17 21:07:46 +0000

    comments

--- VehicleRentalApp/Users.cs
+++ VehicleRentalApp/Users.cs
@@ -9,14 +9,16 @@ namespace VehicleRentalApp
 {
     public class Users
     {
+        // JsonInclude attribute used to tell the JSON serialiser knows to store these variables in the file.
         [JsonInclude] private int UserID;
         [JsonInclude] private string FirstName;
         [JsonInclude] private string LastName;
         [JsonInclude] private string Email;
         [JsonInclude] private string Password;
-        [JsonInclude] private List<int> OwnVehicles = new List<int>();
-        [JsonInclude] private List<int> RentedVehicles = new List<int>();
+        [JsonInclude] private List<int> OwnVehicles = new List<int>(); // To store the id / key of the users vehicles.
+        [JsonInclude] private List<int> RentedVehicles = new List<int>(); // To store the id / key of the vehicles the user has rented.
 
+        // JsonConstructor attribute used to tell the JSON deserializer to use the empty constructor to intialise objects.
         [JsonConstructor]
         public Users() { }
         public Users(int id, string fName, string lName, string email, string password)
@@ -26,6 +28,7 @@ namespace VehicleRentalApp
             LastName = lName;
             Email = email;
             Password = password;
+            // Both lists are empty by defualt
         }
 
         // Adding and removing items from the owned and rented vehicle lists. 
@@ -55,7 +58,7 @@ namespace VehicleRentalApp
         public List<int> GetOwnVehicles() { return OwnVehicles; }
         public List<int> GetRentedVehicles() {return RentedVehicles;}
 
-        // Checks if vehicle id is stored in either list. 
+        // Checks the lists for if they contain the argument passed in.
         public bool CheckOwnVehicles(int id)
         {
             if (OwnVehicles == null || !OwnVehicles.Contains(id)) return false;
